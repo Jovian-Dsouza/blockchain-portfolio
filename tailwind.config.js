d931419e64d69aaf71daba720cb0e730ca6890cd
Jovian Dsouza <dsouzajovian123@gmai.com>
2023-06-24 12:33:54 +0530

    Add frontend for website

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1020px",
+      xl: "1440px",
+    },
+    extend: {
+      fontFamily: ["Poppins", "sans-serif"],
+      colors: {
+        grayishBlack: '#2d2e32',
+        darkishBlue: '#147efb',
+        grayishWhite: '#f9f9f9',
+      },
+      animation: {
+        'spin-slow': 'spin 9s linear infinite',
+      }
+    },
   },
   plugins: [],
-}
-
+};
